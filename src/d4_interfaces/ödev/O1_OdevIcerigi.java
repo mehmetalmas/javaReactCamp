99aa6bcb13c561d67e89b7cf683504a42001e609
Mehmet Almas <m.almas63@gmail.com>
2024-01-03 23:36:42 +0100

    030124-2

---
+++ src/d4_interfaces/ödev/O1_OdevIcerigi.java
@@ -0,0 +1,20 @@
+package d4_interfaces.ödev;
+
+public class O1_OdevIcerigi {
+
+
+    /*
+
+
+
+    Kahve dükkanlari icin müsteri yönetimi yapan bir sistem yazmak istiyoruz
+
+    Starbucks ve Nero Firmasi icin calisiyoruz, iki firma da müsterilerini ver tabanina kaydetmek istiyor
+    Starbucks müsterilerin kaydederken, mutlaka mernis dogrulamasi istiyor. Nero müsterileri kaydederken
+    böyle bir sey istemiyor. Starbuks müsteriler icin her kahve aliminda yildiz kazandirmak istiyor
+
+
+
+
+     */
+}
